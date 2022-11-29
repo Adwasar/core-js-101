250a6e0f9d9ab554df9ad8273a2be5d9b045f66e
Adwasar <user@Users-MacBook-Pro.local>
2022-11-29 18:23:52 +0200

    feat: solve "getMovingSum"

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -235,8 +235,15 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  let sum = 0;
+
+  return arr.reduce((total, elem, i) => {
+    sum += elem;
+    total.push(arr[i - 1] ? sum : elem);
+
+    return total;
+  }, []);
 }
 
 /**
