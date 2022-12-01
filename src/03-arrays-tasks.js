4433a25c6640cd2481e6dec47ce7b5a407b234bb
Adwasar <user@Users-MacBook-Pro.local>
2022-12-01 19:34:25 +0200

    fead: solve "findAllOccurrences"

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -384,8 +384,13 @@ function getFalsyValuesCount(arr) {
  *    [ null, undefined, null ], null => 2
  *    [ true, 0, 1, 'true' ], true => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  return arr.reduce((acc, elem) => {
+    if (elem === item) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
 }
 
 /**
