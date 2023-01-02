03adc5a0d6e76dec680602f987f734f44d4fbddd
Adwasar <user@Users-MacBook-Pro.local>
2023-01-02 19:28:04 +0200

    feat: solve propagateItemsByPositionIndex()

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -276,9 +276,13 @@ function getSecondItems(arr) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
-  //! pass =====================================
+function propagateItemsByPositionIndex(arr) {
+  return arr.reduce((acc, item, i) => (
+    acc.concat(Array.from(
+      { length: i + 1 },
+      () => item,
+    ))
+  ), []);
 }
 
 
